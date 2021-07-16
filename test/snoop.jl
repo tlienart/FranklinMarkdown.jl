e11627fae3b2925c9780848cfad82dee4f8da3a9
Thibaut Lienart <tlienart@me.com>
2021-07-16 12:53:22 +0200

    small edit

--- test/snoop.jl
+++ test/snoop.jl
@@ -6,3 +6,5 @@ itrigs = inference_triggers(tinf)
 fitrigs = filter(itrig -> itrig.callerframes[end].linfo.def.module === FranklinParser, itrigs)
 
 # Jan 30, 3 triggers, can probably ignore.
+# July 16: itrigs has 4 elements for Base._* (probably irrelevant)
+#          fitrigs has 0 elements
